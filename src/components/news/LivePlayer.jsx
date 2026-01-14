46494442ef75211e494afa250b2730118100d93b
רובין שבי <seyorlayla@gmail.com>
2026-01-14 08:15:50 +0000

    File changes

--- src/components/news/LivePlayer.jsx
+++ src/components/news/LivePlayer.jsx
@@ -406,7 +406,7 @@ export default function LivePlayer({
             </div>
           </div>
         </div>
-      </div>
+
         {/* Logo Promo Animation */}
         {showPromo && (
           <motion.div
