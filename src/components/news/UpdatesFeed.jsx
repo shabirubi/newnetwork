851a903ecd8752b58d54ac400fdb9885177d6374
רובין שבי <seyorlayla@gmail.com>
2026-01-17 20:51:29 +0000

    File changes

--- src/components/news/UpdatesFeed.jsx
+++ src/components/news/UpdatesFeed.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "../../utils";
