5993d76bc882a6d0cd4fb43c949b42d5a9ca436c
רובין שבי <seyorlayla@gmail.com>
2026-01-22 21:26:49 +0000

    File changes

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -16,7 +16,6 @@ import moment from "moment";
 
 import NewsCard from "../components/news/NewsCard";
 import CommentsSection from "../components/article/CommentsSection";
-import ReporterArticlePresentation from "../components/news/ReporterArticlePresentation";
 
 const categoryLabels = {
   breaking: "חדשות חמות",
