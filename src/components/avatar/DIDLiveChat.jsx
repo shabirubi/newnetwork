701ae945774123a8353a71457d1f94d4856d90e4
רובין שבי <seyorlayla@gmail.com>
2026-02-12 14:16:05 +0000

    File changes

--- src/components/avatar/DIDLiveChat.jsx
+++ src/components/avatar/DIDLiveChat.jsx
@@ -107,7 +107,7 @@ export default function DIDLiveChat({ isOpen, onClose }) {
                 position: 'absolute',
                 top: '50%',
                 left: '50%',
-                transform: 'translate(-50%, -50%) scale(1.4)',
+                transform: 'translate(-50%, -50%) scale(1.0)',
                 width: '100%',
                 height: '100%',
                 maxWidth: '100%',
